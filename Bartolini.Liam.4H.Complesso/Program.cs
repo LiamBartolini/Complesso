b9f314b4b5a1eb0ab29b214e60e17f0ffd5e358d
LiamBartolini <liambartolini@gmail.com>
2020-09-25 17:29:08 +0200

    Sistemato output

--- Bartolini.Liam.4H.Complesso/Program.cs
+++ Bartolini.Liam.4H.Complesso/Program.cs
@@ -11,31 +11,21 @@ namespace Bartolini.Liam._4H.Complesso
             Complesso z1 = new Complesso(3, 5);
             Complesso z2 = new Complesso(2, 4);
             Complesso zr = new Complesso();
+            string strChoice;
 
-            Console.WriteLine("\r\nSeleziona l'operazione");
+            Console.WriteLine("Seleziona l'operazione");
             Console.WriteLine("1.Somma\r\n2.Sottrazione\r\n3.Moltiplicazione\r\n4.Divisione");
-            string strChoice = Console.ReadLine();
+            while(true)
+            {
+                strChoice = Console.ReadLine();
+                if (strChoice == "1" || strChoice == "2" || strChoice == "3" || strChoice == "4")
+                    break;
+            }
 
-            if(strChoice == "1")
-            {
-                zr.Somma(z1, z2);
-                Console.WriteLine($"La somma dei due numeri è {zr.reale} + {zr.immaginaria}i");
-            }
-            if(strChoice == "2")
-            {
-                zr.Sottrazione(z1, z2);
-                Console.WriteLine($"La sottrazione dei due numeri è {zr.reale} + {zr.immaginaria}i");
-            }
-            if(strChoice == "3")
-            {
-                zr.Prodotto(z1, z2);
-                Console.WriteLine($"Il prodotto dei due numeri è {zr.reale} + {zr.immaginaria}i");
-            }
-            if(strChoice == "4")
-            {
-                zr.Rapporto(z1, z2);
-                Console.WriteLine($"Il rapporto dei due numeri è: {zr.reale} + {zr.immaginaria}i");
-            }
+            if(zr.immaginaria >= 0)
+                Console.WriteLine($"Il risultato della {strChoice} è: {zr.reale}+{zr.immaginaria}i");
+            else
+                Console.WriteLine($"Il risultato della {strChoice} è: {zr.reale}{zr.immaginaria}i");
         }
     }
 
