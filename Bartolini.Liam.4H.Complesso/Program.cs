0b7a4ec20a6570827725321f1649d938d5f87aa5
LiamBartolini <liambartolini@gmail.com>
2020-09-29 15:36:01 +0200

    Aggiunti metodi Fi, Assoluto

--- Bartolini.Liam.4H.Complesso/Program.cs
+++ Bartolini.Liam.4H.Complesso/Program.cs
@@ -8,24 +8,23 @@ namespace Bartolini.Liam._4H.Complesso
         {
             Console.WriteLine("Classe complesso");
 
-            Complesso z1 = new Complesso(3, 5);
-            Complesso z2 = new Complesso(2, 4);
+            Complesso z1 = new Complesso(4, 5, 0, 0);
+            Complesso z2 = new Complesso(2, 1, 0, 0);
             Complesso zr = new Complesso();
-            string strChoice;
 
-            Console.WriteLine("Seleziona l'operazione");
+            Console.WriteLine("\r\nSeleziona l'operazione");
             Console.WriteLine("1.Somma\r\n2.Sottrazione\r\n3.Moltiplicazione\r\n4.Divisione");
-            while(true)
-            {
-                strChoice = Console.ReadLine();
-                if (strChoice == "1" || strChoice == "2" || strChoice == "3" || strChoice == "4")
-                    break;
-            }
+            string strChoice = Console.ReadLine();
 
-            if(zr.immaginaria >= 0)
-                Console.WriteLine($"Il risultato della {strChoice} è: {zr.reale}+{zr.immaginaria}i");
+            if (zr.immaginaria >= 0)
+                Console.WriteLine($"Il risultato del calcolo è: {zr.reale} + {zr.immaginaria}i");
             else
-                Console.WriteLine($"Il risultato della {strChoice} è: {zr.reale}{zr.immaginaria}i");
+                Console.WriteLine($"Il risultato del calcolo è: {zr.reale} {zr.immaginaria}i");
+
+            zr.Assoluto(z1);
+            Console.WriteLine($"Il valore assoluto di {z1.reale} + {z1.immaginaria}i è |{zr.assoluto:n2}|");
+            zr.Fi(z1);
+            Console.WriteLine($"Il valore dell'angolo φ di {z1.reale} + {z1.immaginaria}i = {zr.fi:n2}°");
         }
     }
 
@@ -33,16 +32,29 @@ namespace Bartolini.Liam._4H.Complesso
     {
         public double reale;
         public double immaginaria;
+        public double assoluto;
+        public double fi;
 
         public Complesso()
         {
 
         }
 
-        public Complesso(double a, double b)
+        public Complesso(double a, double b, double c, double d)
         {
             reale = a;
             immaginaria = b;
+        }
+
+        public void Fi(Complesso w1)
+        {
+            //calcolo l'arcotangente di un numero in radianti e lo converto in gradi
+            fi = Math.Atan(w1.immaginaria / w1.reale) * (180 /Math.PI);
+        }
+
+        public void Assoluto(Complesso w1)
+        {
+            assoluto = Math.Sqrt(Math.Pow(w1.reale, 2)+ Math.Pow(w1.immaginaria, 2));
         }
 
         public void Somma(Complesso w1, Complesso w2)
